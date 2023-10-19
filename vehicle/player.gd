6c055e6dd2fbc243cdb0dcb878acfa1cf8d9106b
LeBulldoge <alexey13031@gmail.com>
2023-10-18 20:19:09 -0700

    Brake lights turn on when reversing

--- vehicle/player.gd
+++ vehicle/player.gd
@@ -3,9 +3,19 @@ extends Vehicle
 
 
 func _input(_event):
-    self.drive_dir = Input.get_axis("decelerate", "accelerate")
+    var new_drive_dir = Input.get_axis("decelerate", "accelerate")
     self.steer_dir = Input.get_axis("steer_left", "steer_right")
+
+    if new_drive_dir != self.drive_dir:
+        switch_brake_lights(new_drive_dir < 0)
+
+    self.drive_dir = new_drive_dir
 
 
 func _physics_process(delta):
     self.move_vehicle(delta)
+
+
+func switch_brake_lights(value: bool):
+    $BrakeLightLeft.enabled = value
+    $BrakeLightRight.enabled = value
