be8c712f1f750a2a8e80f1b0776b05179d26278d
Alex Bulldog <alexey13031@gmail.com>
2023-10-20 15:15:14 -0700

    Better UpgradeList structure

--- ui/level_up_screen.gd
+++ ui/level_up_screen.gd
@@ -1,8 +1,7 @@
 extends CanvasLayer
 
 
-@onready var upgrade_list = $MarginContainer/Panel/MarginContainer/VBoxContainer/UpgradeList
-@onready var upgrade_desc =$MarginContainer/Panel/MarginContainer/VBoxContainer/Label2
+@onready var upgrade_list = $MarginContainer/Panel/MarginContainer/VBoxContainer/UpgradeList as UpgradeList
 
 
 signal upgrade_selected(idx: int)
@@ -11,15 +10,13 @@ signal upgrade_selected(idx: int)
 func add_upgrade_item(icon: Texture2D, description: String):
     upgrade_list.add_upgrade_item(icon, description)
 
+
 func choose_upgrade() -> Signal:
     show()
+    upgrade_list.set_column_width($MarginContainer.size.x)
     return upgrade_selected
 
 
 func _on_upgrade_list_upgrade_selected(idx: int, description: String):
     hide()
     upgrade_selected.emit(idx)
-
-
-func _on_upgrade_list_item_hovered(_idx: int, description: String):
-    upgrade_desc.text = description
--- ui/upgrade_list.gd
+++ ui/upgrade_list.gd
@@ -1,31 +1,31 @@
-extends ItemList
+class_name UpgradeList
+extends VBoxContainer
 
 
 signal upgrade_selected(idx: int, description: String)
-signal item_hovered(idx: int, description: String)
+
 
 func add_upgrade_item(icon: Texture2D, description: String):
-    var idx = add_icon_item(icon)
-    set_item_metadata(idx, description)
+    var idx = $List.add_icon_item(icon)
+    $List.set_item_metadata(idx, description)
 
-
-func _on_gui_input(event: InputEvent):
+func _on_list_gui_input(event: InputEvent):
     if not event is InputEventMouseMotion:
         return
 
-    var item = get_item_at_position(get_local_mouse_position(), true)
+    var item = $List.get_item_at_position(get_local_mouse_position(), true)
     if item == -1:
         return
 
-    select(item, true)
-    item_selected.emit(item)
+    $List.select(item, true)
+    $List.item_selected.emit(item)
 
 
-func _on_item_selected(index: int):
-    var desc = get_item_metadata(index)
-    item_hovered.emit(index, desc)
+func _on_list_item_selected(index: int):
+    var desc = $List.get_item_metadata(index)
+    $Description.text = desc
 
 
-func _on_item_clicked(index: int, _at_position, _mouse_button_index):
-    var desc = get_item_metadata(index)
+func _on_list_item_clicked(index: int, _at_position, _mouse_button_index):
+    var desc = $List.get_item_metadata(index)
     upgrade_selected.emit(index, desc)
