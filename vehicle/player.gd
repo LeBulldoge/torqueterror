48f14df3011452c726aec5f50110f81ce37684b7
Alex Bulldog <alexey13031@gmail.com>
2023-10-22 00:05:41 -0700

    Added a player hit sound

--- vehicle/player.gd
+++ vehicle/player.gd
@@ -69,6 +69,6 @@ func _on_experience_component_body_entered(body: RigidBody2D):
         body.queue_free()
 
 
-
 func _on_hit_box_component_pos_damage_taken(damage: float, from: Vector2):
     apply_impulse(from.direction_to(global_position) * damage, from)
+    $PlayerHitSound.play()
